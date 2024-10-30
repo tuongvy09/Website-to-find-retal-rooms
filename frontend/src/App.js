0b6bc47f7b118283d6656df535aadac950910c37
atiets <nguyenanhtuyet03.nbk@gmail.com>
2024-10-30 18:50:40 +0700

    read news list, news detail, add news

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,79 @@
 import React from 'react';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header'; 
+import Home from './components/User/Home/Home';
+import Header from './components/User/Header/Header'; 
 import Footer from './components/Footer/Footer'; 
 import AdminDashboard from './components/Admin/AdminDashboard/AdminDashboard';
 import AdminHeader from './components/Admin/AdminHeader/AdminHeader';
 import ManageUsers from './components/Admin/ManageUsers/ManageUsers'; 
-import AddPost from './components/Post/AddPost';
+import AddPost from './components/User/Post/AddPost';
+import NewsManagement from './components/Admin/News/NewsManagement/NewsManagement';
+import NewsDetail from './components/Admin/News/NewsDetail/NewsDetail';
+import NewsForm from './components/Admin/News/NewsForm/NewsForm';
+import NewsList from './components/Admin/News/NewsList/NewsList';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Header />
-            <Home />
-            <Footer />
-          </>
-        } />
-        <Route path="/admin-dashboard" element={
-          <>
-            <AdminHeader />
-            <AdminDashboard />
-            <Footer />
-          </>
-        } />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/AddPost" element={<AddPost />} />
-        <Route path="/manage-users" element={
-          <>
-            <AdminHeader />
-            <ManageUsers />
-            <Footer />
-          </>
-        } />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={
+            <div className="content-container">
+              <Header />
+              <Home />
+            </div>
+          } />
+          <Route path="/admin-dashboard" element={
+            <div className="content-container">
+              <AdminHeader />
+              <AdminDashboard />
+            </div>
+          } />
+
+          <Route path="/manage-news" element={
+            <div className="content-container">
+              <AdminHeader />
+              <NewsManagement />
+            </div>
+          } />
+
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/AddPost" element={<AddPost />} />
+
+          <Route path="/manage-users/news/" element={
+            <div className="content-container">
+              <AdminHeader />
+              <NewsList />
+            </div>
+          } />
+
+          <Route path="/manage-users/news/" element={
+            <div className="content-container">
+              <AdminHeader />
+              <NewsForm />
+            </div>
+          } />
+
+          <Route path="/manage-users/news/:id" element={
+            <div className="content-container">
+              <AdminHeader />
+              <NewsDetail />
+            </div>
+          } />
+
+          <Route path="/manage-users" element={
+            <div className="content-container">
+              <AdminHeader />
+              <ManageUsers />
+            </div>
+          } />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
