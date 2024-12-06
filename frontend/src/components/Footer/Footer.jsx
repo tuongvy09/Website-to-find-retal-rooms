d8549fa00be21f347e8ce9febfcf2e6d4d1c0fcc
atiets <nguyenanhtuyet03.nbk@gmail.com>
2024-12-06 16:15:46 +0700

    fix layout

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -46,15 +46,20 @@ const Footer = () => {
         <Typography variant="body2">Contact us</Typography>
       </Box>
 
-      {/* <Box className="footer-newsletter">
-        <Typography variant="h6">Bản tin</Typography>
-        <Typography variant="body2">Đăng ký ngay để nhận bản tin mới nhất.</Typography>
-        <TextField label="Email address" variant="outlined" className="email-input" />
-        <Button className="subscribe-button">Đăng ký</Button>
-      </Box> */}
+      <Box className="footer-links">
+        <Typography variant="h6">Liên kết</Typography>
+        <Typography variant="body2">About us</Typography>
+        <Typography variant="body2">Community Blog</Typography>
+        <Typography variant="body2">Work with Us</Typography>
+        <Typography variant="body2">Privacy Policy</Typography>
+        <Typography variant="body2">Contact us</Typography>
+      </Box>
 
       <Box className="footer-contact">
-        <Typography variant="h6">Liên hệ</Typography>
+        {/* <Typography variant="h6">Liên hệ</Typography>
+        <Typography variant="body2">Đăng ký ngay để nhận bản tin mới nhất.</Typography>
+        <TextField label="Email address" variant="outlined" className="email-input" /> */}
+        {/* <Button className="subscribe-button">Đăng ký</Button> */}
         <Typography variant="body2">Số điện thoại: (+84) 0313-728-397</Typography>
         <Typography variant="body2">Email: PhongTroXinh@gmail.com</Typography>
         <Typography variant="body2">Địa chỉ: 01 Đ. Võ Văn Ngân, Linh Chiểu, Thủ Đức, Hồ Chí Minh</Typography>
